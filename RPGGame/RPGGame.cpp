bca7f8ff61f335f017fdf0c46843f7103dea848b
Stuart Burfield <stuart.burfield@ad.aie.edu>
2016-03-16 10:31:18 +1030

    Finished unit testing, output to text file

--- RPGGame/RPGGame.cpp
+++ RPGGame/RPGGame.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "String.h"
 #include <string>
+#include <fstream>
 
 int main()
 {
@@ -10,42 +11,49 @@ int main()
 	String heroName4("Something");	
 	bool testPassed = false;
 
-	// Test Length() function
-	/*
-	if (heroName.Length() == 7)
+	std::fstream file;
+	file.open("UnitTest.txt", std::ios_base::app);
+
+	// Test Length() function	
+	/*if (heroName.Length() == 7)
 	{
 		testPassed = true;
-		std::cout << "Test passed.\n\n";
+		std::cout << "Test 0 passed.\n\n";
+		file << "Test 0\tLength()\tSuccessful\n";		
 	}
 	else
 	{
 		testPassed = false;
-		std::cout << "Test failed. \n\n";
-	}
-	*/
+		std::cout << "Test 0 failed. \n\n";
+		file << "Test 0\tLength()\tFailed\n";
+	}	*/
 
 	// Test CharacterAtIndex() function
 	/*if (heroName.CharacterAtIndex(0) == 'B')
 	{
 		testPassed = true;
-		std::cout << "Test passed. \n\n";
+		std::cout << "Test 1 passed. \n\n";
+		file << "Test 1\tCharacterAtIndex()\tSuccessful\n";
 	}
 	else
 	{
 		testPassed = false;
-		std::cout << "Test failed. \n\n";
+		std::cout << "Test 1 failed. \n\n";
+		file << "Test 1\tCharacterAtIndex()\tFailed\n";
 	}*/
 
 	// Test the ability to compare 2 string classes, using overloaded == operator
 	/*if (heroName == heroName2)
 	{
 		testPassed = true;
-		std::cout << "Test passed. \n\n";
+		std::cout << "Test 2 passed. \n\n";
+		file << "Test 2\tCompare 2 string classes\tSuccessful\n";
 	}
 	else
 	{
 		testPassed = false;
-		std::cout << "Test failed. \n\n";
+		std::cout << "Test 2 failed. \n\n";
+		file << "Test 2\tCompare 2 string classes\tFailed\n";
 	}*/
 
 	// Testing the ability to append one string to another
@@ -53,29 +61,34 @@ int main()
 	if (heroName3 == "BarnabyBarnaby")
 	{
 		testPassed = true;
-		std::cout << "Test passed. \n\n";
+		std::cout << "Test 3 passed. \n\n";
+		file << "Test 3\tAppend one string to another\tSuccessful\n";
 	}
 	else
 	{
 		testPassed = false;
-		std::cout << "Test failed. \n\n";
-	}*/
+		std::cout << "Test 3 failed. \n\n";
+		file << "Test 3\tAppend one string to another\tFailed\n";
+	}
+
+	String heroNameJunior("Junior");*/
 
 	// Testing the abbility to prepend one string to another
-	/*heroName4 = heroName3 + heroName;
+	/*heroName4 = heroNameJunior + heroName;
 	if (heroName4 == "JuniorBarnaby")
 	{
 		testPassed = true;
-		std::cout << "Test passed. \n\n";
+		std::cout << "Test 4 passed. \n\n";
+		file << "Test 4\tPrepend one string to another\tSuccessful\n";
 	}
 	else
 	{
 		testPassed = false;
-		std::cout << "Test failed. \n\n";
+		std::cout << "Test 4 failed. \n\n";
+		file << "Test 4\tPrepend one string to another\tFailed\n";
 	}*/
 
 	// Testing GetString(), the ability to return the string as a const char*
-	//std::cout << heroName.GetString();
 	/*char testString1[256] = "Barnaby";
 	const char* testString2 = { heroName.GetString() };
 	int matches = 0;
@@ -83,12 +96,14 @@ int main()
 	if (matches == 0)
 	{
 		testPassed = true;
-		std::cout << "Test passed. \n\n";
+		std::cout << "Test 5 passed. \n\n";
+		file << "Test 5\tGetString()\tSuccessful\n";
 	}
 	else
 	{
 		testPassed = false;
-		std::cout << "Test failed. \n\n";
+		std::cout << "Test 5 failed. \n\n";
+		file << "Test 5\tGetString()\tFailed\n";
 	}*/
 	
 	// Testing LowCaseDuplicate(), which allows you to make a duplicate of a string class but in lower case
@@ -96,12 +111,14 @@ int main()
 	if (heroName4 == "barnaby")
 	{
 		testPassed = true;
-		std::cout << "Test passed. \n\n";
+		std::cout << "Test 6 passed. \n\n";
+		file << "Test 6\tLowCaseDuplicate()\tSuccessful\n";
 	}
 	else
 	{
 		testPassed = false;
-		std::cout << "Test failed. \n\n";
+		std::cout << "Test 6 failed. \n\n";
+		file << "Test 6\tLowCaseDuplicate()\tFailed\n";
 	}*/
 
 	// Testing UpperCaseDuplicate(), which allows you to make a duplicate of a string class but in lower case
@@ -109,41 +126,45 @@ int main()
 	if (heroName4 == "BARNABY")
 	{
 		testPassed = true;
-		std::cout << "Test passed. \n\n";
+		std::cout << "Test 7 passed. \n\n";
+		file << "Test 7\tUpperCaseDuplicate()\tSuccessful\n";
 	}
 	else
 	{
 		testPassed = false;
 		std::cout << "Test failed. \n\n";
+		file << "Test 7\tUpperCaseDuplicate()\tFailed\n";
 	}*/
 
 	// Testing the SubString() function, if it returns the correct starting index of a String Class / Sub-string
-	/*String heroName5("nab");	
-	std::cout << heroName.SubString(heroName5) << "\n";
+	/*String heroName5("nab");		
 	if (heroName.SubString(heroName5) == 3)
 	{
 		testPassed = true;
-		std::cout << "Test passed. \n\n";
+		std::cout << "Test 8 passed. \n\n";
+		file << "Test 8\tSubString()\tSuccessful\n";
 	}
 	else
 	{
 		testPassed = false;
 		std::cout << "Test failed. \n\n";
+		file << "Test 8\tSubString()\tFailed\n";
 	}*/
 
-	// Testing the ability to find a substring within a string by returning the starting index, 
+	// SubStringFromIndex(), testing the ability to find a substring within a string by returning the starting index, 
 	// starting from a certin index within the string
-	/*String heroName6("nab");
-	std::cout << heroName.SubStringFromIndex(heroName6, 1) << "\n";
+	/*String heroName6("nab");	
 	if (heroName.SubStringFromIndex(heroName6, 1) == 3)
 	{
 		testPassed = true;
-		std::cout << "Test passed. \n\n";
+		std::cout << "Test 9 passed. \n\n";
+		file << "Test 9\tSubStringFromIndex()\tSuccessful\n";
 	}
 	else
 	{
 		testPassed = false;
 		std::cout << "Test failed. \n\n";
+		file << "Test 9\tSubStringFromIndex()\tFailed\n";
 	}*/
 
 	// Testing ReplaceSubString() function to replace a sub-string within the string class with another sub-string
@@ -153,12 +174,14 @@ int main()
 	if (heroName == "Bacarrotby")
 	{
 		testPassed = true;
-		std::cout << "Test passed. \n\n";
+		std::cout << "Test 10 passed. \n\n";
+		file << "Test 10\tReplaceSubString()\tSuccessful\n";
 	}
 	else
 	{
 		testPassed = false;
 		std::cout << "Test failed. \n\n";
+		file << "Test 10\tReplaceSubString()\tFailed\n";
 	}*/
 
 	// Testing Set() function to set the string to something different	
@@ -166,12 +189,14 @@ int main()
 	if (heroName == "Chocolate")
 	{
 		testPassed = true;
-		std::cout << "Test passed. \n\n";
+		std::cout << "Test 11 passed. \n\n";
+		file << "Test 11\tSet()\tSuccessful\n";
 	}
 	else
 	{
 		testPassed = false;
-		std::cout << "Test failed. \n\n";
+		std::cout << "Test 11 failed. \n\n";
+		file << "Test 11\tSet()\tFailed\n";
 	}*/
 
 	system("PAUSE");
